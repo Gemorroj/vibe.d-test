55ef0e5b45bcb10f742183435c41344b957032d9
Gemorroj <wapinet@mail.ru>
2019-01-05 10:05:09 +0300

    upd example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vibe.d-test
 testing vibe.d framework
+
+
+see https://github.com/vibe-d/vibe.d/issues/2243#issuecomment-447927415
--- source/app.d
+++ source/app.d
@@ -5,13 +5,29 @@ void main()
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+	settings.errorPageHandler = toDelegate(&errorPage);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	/*settings.tlsContext = createTLSContext(TLSContextKind.server);
+	settings.tlsContext.useCertificateChainFile("server-cert.pem");
+	settings.tlsContext.usePrivateKeyFile("server-key.pem");*/
+
+	auto router = new URLRouter;
+	router.get("/hello", &hello);
+	router.get("/", staticTemplate!"index.dt");
+	router.get("*", serveStaticFiles("./public/"));
+
+	listenHTTP(settings, router);
+
+	logInfo("HTTP server started on http://127.0.0.1:8080");
 	runApplication();
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
 {
 	res.writeBody("Hello, World!");
+}
+
+void errorPage(HTTPServerRequest req, HTTPServerResponse res, HTTPServerErrorInfo error)
+{
+	res.writeBody(error.message ~ "\n" ~ error.debugMessage);
 }
